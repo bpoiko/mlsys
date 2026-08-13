4347be69c00c809a9632878367cfbfd938d0ba97
Braylon <epicb35@gmail.com>
2026-08-12 22:14:53 -0700

    getting started with pointers

---
+++ 01-c-fundamentals/pointers/pointers.c
@@ -0,0 +1,12 @@
+int main(){
+    int age = 43;
+
+    printf("%d", age); //  value of age
+    printf("%p", age); // pointer of age
+
+
+    int* ptr = &age; // pointer variable, stores the address of age
+
+
+    return 0;
+}
